e6fcec89118e1d6e750f06f26e095e891cc06e15
prateep.gedupudi <prateep.gedupudi@gmail.com>
2015-12-18 14:35:38 +0530

    Changed URL

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -31,7 +31,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/prateep.gedupudi@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
